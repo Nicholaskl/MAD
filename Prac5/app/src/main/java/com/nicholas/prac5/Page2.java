7eec4f02582390162d470a357f8ff1a068cb0d2c
Nicholas2252 <33146327+Nicholas2252@users.noreply.github.com>
2020-10-02 17:51:07 +0800

    Prac 5 Finished

--- Prac5/app/src/main/java/com/nicholas/prac5/Page2.java
+++ Prac5/app/src/main/java/com/nicholas/prac5/Page2.java
@@ -1,28 +1,40 @@
 package com.nicholas.prac5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.service.controls.Control;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.provider.ContactsContract.CommonDataKinds.Email;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+
+import java.io.File;
 
 public class Page2 extends AppCompatActivity {
-    private ImageView iv;
-    private Button photoButton, contactButton;
+    private ImageView iv, bigPhotoView;
+    private Button photoButton, contactButton, bigPhoto;
     private TextView IdText, nameText, emailText, phoneText;
     private static final int REQUEST_THUMBNAIL = 1;
     private static final int REQUEST_CONTACT = 2;
+    private static final int REQUEST_PHOTO = 3;
     private Intent thumbnailPhotoIntent, contactIntent;
+    private File photoFile;
+    private Intent photoIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +45,34 @@ public class Page2 extends AppCompatActivity {
 
         photoButton = findViewById(R.id.photoButton);
         contactButton = findViewById(R.id.contactButton);
+        bigPhoto = findViewById(R.id.bigPhoto);
 
         IdText = findViewById(R.id.IdText);
         nameText = findViewById(R.id.nameText);
         emailText = findViewById(R.id.emailText);
         phoneText = findViewById(R.id.phoneText);
+
+        bigPhotoView = findViewById(R.id.bigPhotoView);
+
+
+        photoFile = new File(getFilesDir(), "photo.jpg");
+        photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri cameraUri = FileProvider.getUriForFile(
+                this,
+                "com.nicholas.prac5.fileprovider",
+                photoFile);
+        photoIntent.putExtra(MediaStore.EXTRA_OUTPUT, cameraUri);
+        PackageManager pm = getPackageManager();
+
+        for(ResolveInfo a : pm.queryIntentActivities(
+                photoIntent,
+                PackageManager.MATCH_DEFAULT_ONLY))
+        {
+            grantUriPermission(
+                    a.activityInfo.packageName,
+                    cameraUri,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
 
         photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,10 +91,17 @@ public class Page2 extends AppCompatActivity {
                 startActivityForResult(contactIntent, REQUEST_CONTACT);
             }
         });
+
+        bigPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(photoIntent, REQUEST_PHOTO);
+            }
+        });
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent resultIntent) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent resultIntent) {
         super.onActivityResult(requestCode, resultCode, resultIntent);
 
         if(resultCode == RESULT_OK &&
@@ -74,27 +116,65 @@ public class Page2 extends AppCompatActivity {
             Uri contactUri = resultIntent.getData();
 
             String[] queryFields = new String[] {
-                ContactsContract.Contacts._ID,
-                ContactsContract.Contacts.DISPLAY_NAME
+                    ContactsContract.Contacts._ID,
+                    ContactsContract.Contacts.DISPLAY_NAME
             };
 
-            Cursor c = Page2.this.getContentResolver().query(
+            Cursor c = getContentResolver().query(
                 contactUri, queryFields, null, null, null);
 
             try {
                 if(c.getCount() > 0) {
                     c.moveToFirst();
-                    IdText.setText(c.getInt(0));
-                    nameText.setText(c.getString(1));
-                    phoneText.setText("PP");
-                    /*emailText.setText(c.getString(2));
-                    phoneText.setText(c.getString(3));*/
+                    int id = c.getInt(0);
+                    IdText.setText("ID: " + id);
+                    nameText.setText("Name: " + c.getString(1));
+
+                    Cursor emailC = getContentResolver().query(
+                            Email.CONTENT_URI,
+                            new String[] {Email.ADDRESS},
+                            Email.CONTACT_ID + " = ?",
+                            new String[] {String.valueOf(id)},
+                            null, null
+                    );
+                    try {
+                        if(emailC.getCount() > 0) {
+                            emailC.moveToFirst();
+                            emailText.setText("Email: " + emailC.getString(0));
+                        }
+                    }
+                    finally {
+                        emailC.close();
+                    }
+
+                    Cursor phoneC = getContentResolver().query(
+                            Phone.CONTENT_URI,
+                            new String[] {Phone.NUMBER},
+                            Phone.CONTACT_ID + " = ?",
+                            new String[] {String.valueOf(id)},
+                            null, null
+                    );
+                    try {
+                        if(phoneC.getCount() > 0) {
+                            phoneC.moveToFirst();
+                            phoneText.setText("Phone: " + phoneC.getString(0));
+                        }
+                    }
+                    finally {
+                        emailC.close();
+                    }
                 }
             }
             finally
             {
                 c.close();
             }
+        }
+        else if(resultCode == RESULT_OK &&
+                requestCode == REQUEST_PHOTO)
+        {
+            Bitmap photo = BitmapFactory.decodeFile(photoFile.toString());
+            bigPhotoView.setImageBitmap(photo);
         }
     }
 }
