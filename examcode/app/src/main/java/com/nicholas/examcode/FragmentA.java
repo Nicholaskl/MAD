e2c75460ee3c78ea423b86dc0a440aacb3bfbaaf
Nicholas2252 <33146327+Nicholas2252@users.noreply.github.com>
2020-11-21 11:01:04 +0800

    New :)

---
+++ examcode/app/src/main/java/com/nicholas/examcode/FragmentA.java
@@ -0,0 +1,28 @@
+package com.nicholas.examcode;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentA#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentA extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_a, container, false);
+
+        view.setBackgroundColor(Color.GREEN);
+
+        return view;
+    }
+}
